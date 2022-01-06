1f6fd41d9782e6534a2daf8bb5cb24b54a91ea58
Nicolas Ruiz <syngenruiz2@gmail.com>
2022-01-06 05:25:03 -0600

    added section routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AttendComponent } from './attend/attend.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { FollowComponent } from './follow/follow.component';
+import { GuideComponent } from './guide/guide.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AttendComponent,
+    SponsorsComponent,
+    FollowComponent,
+    GuideComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/attend/attend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attend',
+  templateUrl: './attend.component.html',
+  styleUrls: ['./attend.component.sass']
+})
+export class AttendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
