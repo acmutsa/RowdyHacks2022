e0fbe6a488b19d9abd0f2e20162964b466941e93
Juan Neri <nerijf12@gmail.com>
2022-01-14 10:22:19 -0600

    making changes to the faq and adjusting the  homepage design and adding illustrations

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,4 +1,5 @@
 $(function() {
+    $('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span><span></span></div>');
     $('.show').click( function() {
         $(this).children(".arrows").toggleClass('arrow-down');
         $(this).next().slideToggle();
