a933f2057b801553d8075d33130d2acd92de25af
Juan Neri <nerijf12@gmail.com>
2022-01-23 23:42:21 -0600

    looking into the issue still

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,22 +1,45 @@
 window.onload = function(){
+    var exists = document.getElementById("song");
     buttonAnimation();
     showFAQ();
-    playSong();
+    if(exists)
+        playSong();
 }
 
 function playSong(){
     var song = document.getElementById("song");
     var icon = document.getElementById("volumeIcon");
+    var playPromise = song.play();
 
-    icon.onclick = function(){
-        song.loop = true;
-        if(song.paused){
-            song.play();
-            icon.src = "../assets/images/volume-icon-pink.gif";
-        }else{
+    if (playPromise !== undefined) {
+        playPromise.then(_ => {
+        // Automatic playback started!
+        // Show playing UI.
             song.pause();
-            icon.src = "../assets/images/volume-icon-pink-mute.png";
-        }
+            icon.onclick = function(){
+                song.loop = true;
+                if(song.paused){
+                    song.play();
+                    icon.src = "../assets/images/volume-icon-pink.gif";
+                }else{
+                    song.pause();
+                    icon.src = "../assets/images/volume-icon-pink-mute.png";
+                }
+            }
+        }).catch(error => {
+        // Auto-play was prevented
+        // Show paused UI.
+            icon.onclick = function(){
+                song.loop = true;
+                if(song.paused){
+                    song.play();
+                    icon.src = "../assets/images/volume-icon-pink.gif";
+                }else{
+                    song.pause();
+                    icon.src = "../assets/images/volume-icon-pink-mute.png";
+                }
+            }
+        });
     }
 }
 
