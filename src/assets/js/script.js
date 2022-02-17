87e16226ec44d34b90aecc41adfe5d34564be33f
Juan Neri <nerijf12@gmail.com>
2022-02-17 06:08:16 -0600

    making mentors just reliable on json and not the html for the content for faster input

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,6 +2,8 @@ window.onload = function(){
     var exists = document.getElementById("song");
     var mapExists = document.getElementById("map");
     var randomExists = document.getElementById("random");
+    var mentorholder = document.getElementById("mentorsHolder");
+
     arrowAppear();
     buttonAnimation();
     showFAQ();
@@ -10,12 +12,19 @@ window.onload = function(){
     if(exists){
         playSong();
     }
+
     if(mapExists){
         initMap();
     }
+
     if(randomExists){
+        getMentorsHome();
+    }
+
+    if(mentorholder){
         getMentors();
     }
+
     getCurrentPage();
 }
 
@@ -141,7 +150,7 @@ function initMap() {
     
 }
 
-function getMentors(){
+function getMentorsHome(){
     fetch('/assets/js/mentors.json')
     .then(res => res.json())
     .then((out) => {
@@ -155,7 +164,6 @@ function getMentors(){
             })
             randomNumHolder.push(random);
             var link = out[random].Link;
-            var description = out[random].Description;
             var firstname = out[random].FirstName;
             var lastname = out[random].LastName;
             var title = out[random].Title;
@@ -214,74 +222,110 @@ function getMentors(){
             primaryItem.appendChild(alink);
             $('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span><span></span></div>');
             alink.appendChild(readMore);
-
-            // Above simmulates whats at the bottom at random
-            // <div class="col-primary-inner-container">
-            //     <div class="texture-7op">
-            //       <div class="primary-item">
-            //         <img src="./assets/images/people-guy-3.png" alt="name" />
-            //         <div class="inner-container mentors">
-            //           <div class="item">
-            //             <h1>
-            //               <div class="cyan-background">FirstName</div>
-            //             </h1>
-            //           </div>
-            //           <div class="item">
-            //             <h1>
-            //               <div class="magenta-background">LastName</div>
-            //             </h1>
-            //           </div>
-            //         </div>
-            //         <p>Software Engineer</p>
-            //         <a href="#" class="btn btn-magenta">Read More</a>
-            //       </div>
-            //     </div>
-            //   </div>
-            //   <div class="col-primary-inner-container">
-            //     <div class="texture-7op">
-            //       <div id="random2" class="primary-item">
-            //         <img src="./assets/images/people-guy-2.png" alt="name" />
-            //         <div class="inner-container mentors">
-            //           <div class="item">
-            //             <h1>
-            //               <div class="cyan-background">FirstName</div>
-            //             </h1>
-            //           </div>
-            //           <div class="item">
-            //             <h1>
-            //               <div class="magenta-background">LastName</div>
-            //             </h1>
-            //           </div>
-            //         </div>
-            //         <p>Software Engineer</p>
-            //         <a href="#" class="btn btn-magenta">Read More</a>
-            //       </div>
-            //     </div>
-            //   </div>
-            //   <div class="col-primary-inner-container">
-            //     <div class="texture-7op">
-            //       <div id="random3" class="primary-item">
-            //         <img src="./assets/images/people-gal-1.png" alt="name" />
-            //         <div class="inner-container mentors">
-            //           <div class="item">
-            //             <h1>
-            //               <div class="cyan-background">FirstName</div>
-            //             </h1>
-            //           </div>
-            //           <div class="item">
-            //             <h1>
-            //               <div class="magenta-background">LastName</div>
-            //             </h1>
-            //           </div>
-            //         </div>
-            //         <p>Software Engineer</p>
-            //         <a href="#" class="btn btn-magenta">Read More</a>
-            //       </div>
-            //     </div>
-            //   </div>
         }
     })
     .catch(err => {
+        throw err
+    });
+}
+
+function getMentors(){
+    fetch('/assets/js/mentors.json').then(res => res.json()).then((out) => {
+        for(i = 0; i <= 5; i++){
+            var description = out[i].Description;
+            var firstname = out[i].FirstName;
+            var lastname = out[i].LastName;
+            var title = out[i].Title;
+            var mentorImage = out[i].Image;
+
+            var divHolder = document.getElementById("mentorsHolder");
+            var colPrimaryInnerContainer = document.createElement("div");
+            var texture7op = document.createElement("div");
+            var primaryItem = document.createElement("div");
+            var innercontainer = document.createElement("div");
+            var innercontainer2 = document.createElement("div");
+            var item1 = document.createElement("div");
+            var item2 = document.createElement("div");
+            var item3 = document.createElement("div");
+            var item4 = document.createElement("div");
+            var item5 = document.createElement("div");
+            var yellowBackground = document.createElement("div");
+            var yellowBackground2 = document.createElement("div");
+            var cyanBackground = document.createElement("div");
+            var magentaBackground = document.createElement("div");
+            var spacing = document.createElement("div");
+            var img = document.createElement("img");
+            var header1A = document.createElement("h1");
+            var header1B = document.createElement("h1");
+            var header2A = document.createElement("h2");
+            var header2B = document.createElement("h2");
+            var paragraph = document.createElement("p");
+
+            var FN = document.createTextNode(firstname);
+            var LN = document.createTextNode(lastname);
+            var titleText = document.createTextNode(title);
+            var descText = document.createTextNode(description);
+            var FL = document.createTextNode("Software");
+            var SL = document.createTextNode("Engineer");
+            var nbs = document.createTextNode("\xa0");
+
+            img.src = "../assets/images/mentors/"+mentorImage;
+            innercontainer.className = "inner-container mentors";
+            innercontainer2.className = "inner-container mentors";
+            item1.className = "item";
+            item2.className = "item";
+            item3.className = "item";
+            item4.className = "item";
+            item5.className = "item";
+            spacing.className = "mentors-spacing";
+            yellowBackground.className = "yellow-background";
+            yellowBackground2.className = "yellow-background";
+            cyanBackground.className = "cyan-background";
+            magentaBackground.className = "magenta-background";
+            colPrimaryInnerContainer.className = "col-primary-inner-container mentors";
+            texture7op.className = "texture-7op";
+            primaryItem.className = "primary-item mentors";
+
+            divHolder.appendChild(colPrimaryInnerContainer);
+            colPrimaryInnerContainer.appendChild(texture7op);
+            texture7op.appendChild(primaryItem);
+            primaryItem.appendChild(img);
+            primaryItem.appendChild(innercontainer);
+
+            innercontainer.appendChild(item1);
+            item1.appendChild(header1A);
+            header1A.appendChild(cyanBackground);
+            cyanBackground.appendChild(FN);
+
+            innercontainer.appendChild(item2);
+            item2.appendChild(header1B);
+            header1B.appendChild(magentaBackground);
+            magentaBackground.appendChild(LN);
+
+            // software engineer
+            if(description = "Software Engineer"){
+                primaryItem.appendChild(innercontainer2);
+
+                innercontainer2.appendChild(item3);
+                item3.appendChild(header2A);
+                header2A.appendChild(yellowBackground);
+                yellowBackground.appendChild(FL);
+
+                innercontainer2.appendChild(item4);
+                item4.appendChild(spacing);
+                spacing.appendChild(nbs);
+
+                innercontainer2.appendChild(item5);
+                item5.appendChild(header2B);
+                header2B.appendChild(yellowBackground2);
+                yellowBackground2.appendChild(SL);
+
+                paragraph.appendChild(descText);
+                primaryItem.appendChild(paragraph);
+            }
+
+        }
+    }).catch(err => {
         throw err
     });
 }
